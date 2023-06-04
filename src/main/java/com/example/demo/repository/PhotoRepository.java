c61ee8ff40702acb521719328c7cf81398cfd689
NabilSas <nabilsas91@gmail.com>
2023-06-04 02:29:06 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/repository/PhotoRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Photo;
+
+
+public interface PhotoRepository extends JpaRepository<Photo, Integer> {
+    Photo findById(int id);
+
+
+}
